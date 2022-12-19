f4b5cc721d33cb2e077e63f6540d81fb3cead840
Seolyu <leeseolyu@gmail.com>
2022-12-19 12:08:52 +0900

    1장 리액트 입문 | 배열에 항목 추가하기

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,12 +1,25 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 // import Counter from './Counter';
 // import Hello from './Hello';
 // import Input from './input';
 import UserList from './UserList';
 // import Wrapper from './Wrapper';
+import CreateUser from './CreateUser';
 
 function App() {
-  const users = [
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+  });
+  const { username, email } = inputs;
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+  const [users, setUsers] = useState([
     {
       id: 1,
       username: 'velopert',
@@ -22,15 +35,36 @@ function App() {
       username: 'liz',
       email: 'liz@example.com',
     },
-  ];
+  ]);
 
   const nextId = useRef(4); // useRef 파라미터 값은 .current 기본값
   const onCreate = () => {
-    // 나중에 구현할 배열에 항목 추가 로직
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    setUsers([...users, user]);
+    // setUsers(users.concat(user));
+
+    setInputs({
+      username: '',
+      email: '',
+    });
     nextId.current += 1;
   };
 
-  return <UserList users={users} />;
+  return (
+    <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} />
+    </>
+  );
   // return (
   // <Wrapper>
   //   <Hello name="react" color="red" isSpecial={true} />
